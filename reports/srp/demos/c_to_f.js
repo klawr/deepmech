b65287152d64f27e6edabfc81a7348a5215fc15b
Kai Lawrence <mail@kailawrence.de>
2019-10-08 02:10:56 +0000

    Update c_to_f demo
    The now better represents the tensor idea behind the weights.
    Code is adjusted but may require further explanations...
    
    Because of the last change to move the workspace in docker, the
    appendix code can now input the file instead of copy pasting the
    text.

--- reports/srp/demos/c_to_f.js
+++ reports/srp/demos/c_to_f.js
@@ -1,24 +1,20 @@
 const model = {
-    m: Math.random(),
-    b: Math.random(),
-    h(C) { return C * this.m + this.b; } 
+    w: [Math.random(), Math.random()],
+    h(x) { return this.w.reduce((pre, cur, idx) => pre + x ** idx * cur, 0); }
 }
 
 function y(C) { return C * 1.8 + 32; }
 
-function mse(h, y) { return (h - y)**2 / 2; }
+function mse(h, y) { return (h - y) ** 2 / 2; }
 
 function sgd(model, x) {
-    temp_m = model.m - ((model.h(x) - y(x)) * x);
-    temp_b = model.b - (model.h(x) - y(x));
-    model.m = temp_m;
-    model.b = temp_b;
+    model.w =  model.w.map((weight, idx) => weight - (model.h(x) - y(x)) * x ** idx);
 }
 
 for (i = 0; i < 200; ++i) {
-    const F = Math.random();
-    const loss = mse(model.h(F), y(F));
-    sgd(model, F);
-    if (i % 20 === 0) console.log('loss:', loss);
+    const C = Math.random() * 100;
+    sgd(model, C / 100);
+    if (!(i % 20)) console.log('loss: ', mse(model.h(C), y(C)));
 }
-console.log('m:', model.m, 'b:', model.b)
+
+console.log('weights: ', model.w);
--- reports/srp/demos/c_to_f_adv.js
+++ reports/srp/demos/c_to_f_adv.js
@@ -1,26 +1,20 @@
 const model = {
-    m: Math.random(),
-    n: Math.random(),
-    b: Math.random(),
-    h(C) { return C * this.m + C * C * this.n + this.b; } 
+    w: [Math.random(), Math.random(), Math.random()],
+    h(x) { return this.w.reduce((pre, cur, idx) => pre + x ** idx * cur, 0); }
 }
 
 function y(C) { return C * 1.8 + 32; }
 
-function mse(h, y) { return (h - y)**2 / 2; }
+function mse(h, y) { return (h - y) ** 2 / 2; }
 
 function sgd(model, x) {
-    temp_m = model.m - ((model.h(x) - y(x)) * x);
-    temp_n = model.n - ((model.h(x) - y(x)) * x * x);
-    temp_b = model.b - (model.h(x) - y(x));
-    model.m = temp_m;
-    model.n = temp_n;
-    model.b = temp_b;
+    model.w =  model.w.map((weight, idx) => weight - (model.h(x) - y(x)) * x ** idx);
 }
 
 for (i = 0; i < 4000; ++i) {
-    const F = Math.random();
-    const loss = mse(model.h(F), y(F));
-    sgd(model, F);
+    const C = Math.random() * 100;
+    sgd(model, C / 100);
+    if (!(i % 20)) console.log('loss: ', mse(model.h(C), y(C)));
 }
-console.log('m:', model.m, 'n:', model.n, 'b:', model.b)
+
+console.log('weights: ', model.w);
