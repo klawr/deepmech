7e79aaf9046994fc083907b996752993755c5798
Kai Lawrence <mail@kailawrence.de>
2019-11-03 20:58:05 +0100

    Update what_is_ml

--- reports/srp/demos/c_to_f.js
+++ reports/srp/demos/c_to_f.js
@@ -1,6 +1,8 @@
 const model = {
     w: [Math.random(), Math.random()],
-    h(x) { return this.w.reduce((pre, cur, idx) => pre + x ** idx * cur, 0); }
+    h(x) {
+        return this.w.reduce((pre, cur, idx) => pre + x ** idx * cur, 0);
+    }
 }
 
 function y(C) { return C * 1.8 + 32; }
@@ -8,7 +10,8 @@ function y(C) { return C * 1.8 + 32; }
 function mse(h, y) { return (h - y) ** 2 / 2; }
 
 function sgd(model, x) {
-    model.w =  model.w.map((weight, idx) => weight - (model.h(x) - y(x)) * x ** idx);
+    model.w =  model.w.map((weight, idx) =>
+        weight - (model.h(x) - y(x)) * x ** idx);
 }
 
 for (i = 0; i < 200; ++i) {
