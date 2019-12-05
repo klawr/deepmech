c488f4d9500f21d3d2cb149004ee4f2fdfcd6e8c
Kai Lawrence <mail@kailawrence.de>
2019-12-05 18:57:50 +0100

    Add conv_layer image

--- reports/srp/README.md
+++ reports/srp/README.md
@@ -1,2 +1,7 @@
 # Student research project for deepmech
+
+Please note that code used in this project is run via the Python extension in
+Visual Studio Code.
+If some paths to used libraries in this project aren't fitting the respective
+path, they have to be added accordingly.
 <!-- TODO: insert well thought of title here -->
--- reports/srp/demos/g2.helper.js
+++ reports/srp/demos/g2.helper.js
@@ -68,3 +68,30 @@ g2.prototype.nn_with_bias = function (layerArr, width, height, r) {
     layerArr.map(nodes).reduce(layers, layerArr[0]);
     return this;
 }
+
+g2.prototype.egrid = function ({x, y, s ,ky, kx, padding = 0}) {
+    const p = padding;
+    for (let i=0; i < ky; ++i) {
+        for (let j=0; j < kx; ++j) {
+            let fs = undefined;
+            if (i < p || j < p || i > ky - p - 1 || j > kx - p - 1) {
+                fs = 'grey'
+            }
+            this.rec({x: x + s*j, y: y + s*i, b:s, h:s, fs})
+        }
+    }
+    return this;
+}
+
+g2.prototype.grid_connect = function({x1, y1, s, x2, y2, kernel_size=3, color, ld=[]}) {
+    const dist = s * kernel_size;
+        this.rec({x:x2, y:y2, b: s, h: s, fs:color})
+            .beg({ls:color, ld})
+        .rec({x: x1, y: y1, b: 3*s, h:3*s, lw:5})
+        .lin({x1, y1, x2: x2 + s/2, y2: y2 + s/2})
+        .lin({x1: x1+dist, y1: y1, x2: x2 + s/2, y2: y2 + s/2})
+        .lin({x1: x1, y1: y1+dist, x2: x2 + s/2, y2: y2 + s/2})
+        .lin({x1: x1+dist, y1: y1+dist, x2: x2 + s/2, y2: y2 + s/2})
+        .end();
+    return this;
+}
