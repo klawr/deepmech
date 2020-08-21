6db5068e730919c533494ad485c925109dd6a4c7
Kai Lawrence <mail@kailawrence.de>
2020-08-21 20:00:26 +0200

    Add styles

--- src/deepmech.js
+++ src/deepmech.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import clsx from 'clsx';
+import AppBar from '@material-ui/core/AppBar';
 import Drawer from '@material-ui/core/Drawer';
-import Grid from '@material-ui/core/Grid'
+import Grid from '@material-ui/core/Grid';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
 import IconButton from '@material-ui/core/IconButton';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
@@ -10,7 +15,39 @@ import ClearIcon from '@material-ui/icons/Clear';
 import PauseIcon from '@material-ui/icons/Pause';
 import PlayArrowIcon from '@material-ui/icons/PlayArrow';
 import RotateLeftIcon from '@material-ui/icons/RotateLeft';
-// import { makeStyles, useTheme } from '@material-ui/core/styles';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+
+const drawerWidth = 100;
+
+const useStyle = makeStyles((theme) => ({
+    root: {
+        display: 'flex'
+    },
+    drawer: {
+        width: drawerWidth,
+        flexShrink: 0,
+    },
+    drawerPaper: {
+        width: drawerWidth,
+    },
+    hide: {
+        display: 'none',
+    },
+    appBar: {
+        transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+    },
+    appBarShift: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: drawerWidth,
+        transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+}));
 
 function DeepmechNav() {
     const [open, setOpen] = React.useState(false);
@@ -33,31 +70,45 @@ function DeepmechNav() {
         mecElement.toggleGravity();
     }
 
+    const classes = useStyle();
+
     return (
-        <div>
+        <div className={classes.root}>
             <Drawer
+                className={classes.drawer}
+                classes={{
+                    paper: classes.drawerPaper,
+                }}
                 open={open}
                 anchor="left"
                 variant="persistent">
-                <IconButton onClick={toggleDrawer}>
+                    <List>
+
+                <ListItem onClick={toggleDrawer}>
                     <ChevronLeftIcon />
-                </IconButton>
-                <IconButton onClick={run}>
+                </ListItem>
+                <ListItem onClick={run}>
                     {pausing ? <PlayArrowIcon /> : <PauseIcon />}
-                </IconButton>
-                <IconButton onClick={toggleGravity}>
+                </ListItem>
+                <ListItem onClick={toggleGravity}>
                     g {gravity ? <ClearIcon /> : <ArrowDownwardIcon />}
-                </IconButton>
-                <IconButton onClick={ref.reset}>
+                </ListItem>
+                <ListItem onClick={ref.reset}>
                     <RotateLeftIcon />
-                </IconButton>
+                </ListItem>
+                </List>
             </Drawer>
-            <Grid container direction="row">
-                <IconButton onClick={toggleDrawer}>
-                    <ChevronRightIcon />
-                </IconButton>
-                <h3> 🚧 Work in progress 🚧 </h3>
-            </Grid>
+            <AppBar position="fixed"
+                className={clsx(classes.appBar, { [classes.appBarShift]: open, })}>
+                <Grid container direction="row">
+                    <IconButton
+                        onClick={toggleDrawer}
+                        className={clsx(classes.menuButton, open && classes.hide)} >
+                        <ChevronRightIcon />
+                    </IconButton>
+                    <h3>&nbsp; 🚧 Work in progress 🚧 </h3>
+                </Grid>
+            </AppBar>
         </div>
     );
 }
