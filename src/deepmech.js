5a99fd7d474e574745940c5eae9d22e6f809b270
Kai Lawrence <mail@kailawrence.de>
2020-08-21 10:28:32 +0200

    Add Drawer and Icons

--- src/deepmech.js
+++ src/deepmech.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Button from '@material-ui/core/Button';
+import Drawer from '@material-ui/core/Drawer';
+import IconButton from '@material-ui/core/IconButton';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+// import { makeStyles, useTheme } from '@material-ui/core/styles';
 
-class DeepmechNav extends React.Component {
-    render() {
-        return (
-            <div>
-                <Button onClick={() => mecElement.run()}>
-                    Hello, world!!!!! This is src
-                </Button>
-            </div>
-        );
-    }
+function DeepmechNav() {
+    const [open, setOpen] = React.useState(false);
+
+    const toggleDrawer = () => {
+        setOpen(!open);
+    };
+
+    return (
+        <div>
+            <Drawer
+                open={open}
+                anchor="left"
+                variant="persistent">
+                <IconButton onClick={toggleDrawer}>
+                    <ChevronLeftIcon />
+                </IconButton>
+                <IconButton onClick={() => mecElement.run()}>
+                    <PlayArrowIcon/>
+                </IconButton>
+            </Drawer>
+            <IconButton onClick={toggleDrawer}>
+                <ChevronRightIcon />
+            </IconButton>
+        </div>
+    );
 }
 
-ReactDOM.render(<DeepmechNav/>, document.getElementById('deepmech_nav'))
+ReactDOM.render(<DeepmechNav />, document.getElementById('deepmech_nav'))
