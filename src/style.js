dbc7ba5834c97f645b5405b251f8031d27d7864c
Kai Lawrence <mail@kailawrence.de>
2020-09-08 22:00:02 +0200

    Set rightDrawerWidth according to display width

--- src/style.js
+++ src/style.js
@@ -1,7 +1,7 @@
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 
 const leftDrawerWidth = "auto";
-const rightDrawerWidth = "auto";
+const rightDrawerWidth = Math.min(250, globalThis.innerWidth);
 
 const useStyle = makeStyles((theme) => ({
     root: {
