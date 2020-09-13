546cb83c59a94972604788e33f523a87605e720d
Kai Lawrence <mail@kailawrence.de>
2020-09-13 23:42:36 +0200

    Fix webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-   entry: './src/deepmech.js',
+   entry: './src/DeepmechUI.js',
    output: {
       filename: './dist/deepmech_bundle.js'
    },
