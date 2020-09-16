bda9698ae5fc0239a16b1c79b4785fc23eaa2499
Kai Lawrence <mail@kailawrence.de>
2020-09-16 17:40:00 +0200

    Move Components in Components directory

--- src/Components/DeepmechUI.js
+++ src/Components/DeepmechUI.js
@@ -6,7 +6,7 @@ import IconButton from '@material-ui/core/IconButton';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 
-import { useStyle } from './style';
+import { useStyle } from '../style';
 import { LeftDrawer } from './LeftDrawer';
 import { RightDrawer } from './RightDrawer';
 import { DeepmechDraw } from './DeepmechDraw';
