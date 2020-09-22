18c625cab671de570bda4084b690b7da92a96887
Kai Lawrence <mail@kailawrence.de>
2020-09-22 17:21:38 +0200

    Implement node coordinate updatability
    Input validation still required.
    And maybe add a tolerance for numbers to be rounded...

--- src/Components/Mec2/Nodes.js
+++ src/Components/Mec2/Nodes.js
@@ -1,19 +1,32 @@
 import React from 'react';
-import { Checkbox, } from '@material-ui/core';
+import { Checkbox, InputBase, } from '@material-ui/core';
 import { MecTable } from '..';
 
 export default function Nodes({ mec2, head, elms }) {
     function SanitizedCell({ prop, elm }) {
         const node = mec2._model.nodeById(elm.id);
+
+        function handleUpdate(ref, prop, def = ref[prop]) {
+            const [val, changeVal] = React.useState(def);
+            function handleChange(targetValue) {
+                changeVal(targetValue);
+                ref[prop] = targetValue;
+                mec2._model.reset();
+                mec2.render();
+            };
+    
+            return [val, handleChange];
+        }
+
         // Custom mec2 properties
         if (prop === 'base') {
-            const [checked, toggleCheck] = React.useState(!!node.base);
-            function handleChange(e) {
-                toggleCheck(e.target.checked);
-                node.base = e.target.checked;
-                mec2.render();
-            }
-            return <Checkbox checked={checked} onChange={handleChange} />
+            const [checked, toggleCheck] = handleUpdate(node, prop);
+            return <Checkbox checked={checked} onChange={(e) => toggleCheck(e.target.checked)} />
+        }
+
+        if (prop === 'x' || prop === 'y') {
+            const [value, changeValue] = handleUpdate(node, prop);
+            return <InputBase value={value} onChange={(e) => changeValue(e.target.value)}/> 
         }
 
         if (typeof elm[prop] === "object") {
