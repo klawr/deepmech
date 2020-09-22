e60848b6dbf72b1e043df937aa493042d797596f
Kai Lawrence <mail@kailawrence.de>
2020-09-22 19:23:51 +0200

    Cleanup Nodes

--- src/Components/Mec2/Nodes.js
+++ src/Components/Mec2/Nodes.js
@@ -2,57 +2,49 @@ import React from 'react';
 import { Checkbox, InputBase, } from '@material-ui/core';
 import { MecTable } from '..';
 
+function handleUpdate(ref, prop, fn) {
+    const [val, changeVal] = React.useState(ref[prop]);
+    function handleChange(targetValue) {
+        changeVal(targetValue);
+        ref[prop] = targetValue;
+        if (fn) fn(targetValue);
+        mec2._model.reset();
+        mec2.render();
+    };
+
+    return [val, handleChange];
+}
+
 export default function Nodes({ mec2, head, elms }) {
     function SanitizedCell({ prop, elm }) {
         const node = mec2._model.nodeById(elm.id);
 
-        function handleUpdate(ref, prop, fn) {
-            const [val, changeVal] = React.useState(ref[prop]);
-            function handleChange(targetValue) {
-                changeVal(targetValue);
-                ref[prop] = targetValue;
-                if (fn) fn(targetValue);
-                mec2._model.reset();
-                mec2.render();
-            };
-    
-            return [val, handleChange];
-        }
+        switch (prop) {
+            case 'base':
+                const [checked, toggleCheck] = handleUpdate(node, prop);
+                return <Checkbox
+                    checked={checked}
+                    onChange={(e) => toggleCheck(e.target.checked)} />
+            case 'x':
+            case 'y':
+                const [value, changeValue] = handleUpdate(node, prop);
+                return <InputBase
+                    value={value}
+                    onChange={(e) => changeValue(e.target.value)} />
+            case 'id':
+                function propagateChange(newId) {
+                    mec2._model.constraints.forEach(c => {
+                        if (c.p1 === id) c.p1 = newId;
+                        if (c.p2 === id) c.p2 = newId;
+                    });
+                    mec2._model.views.forEach(v => {
+                        if (v.of === id) v.of = newId;
+                    });
+                }
 
-        // Custom mec2 properties
-        if (prop === 'base') {
-            const [checked, toggleCheck] = handleUpdate(node, prop);
-            return <Checkbox checked={checked} onChange={(e) => toggleCheck(e.target.checked)} />
-        }
-
-        if (prop === 'x' || prop === 'y') {
-            const [value, changeValue] = handleUpdate(node, prop);
-            return <InputBase value={value} onChange={(e) => changeValue(e.target.value)}/> 
-        }
-
-        if (prop === 'id') {
-            function propagateChange(newId) {
-                mec2._model.constraints.forEach(c => {
-                    if (c.p1 === id) c.p1 = newId;
-                    if (c.p2 === id) c.p2 = newId;
-                });
-                mec2._model.views.forEach(v => {
-                    if (v.of === id) v.of = newId;
-                });
-            }
-
-            const [id, changeId] = handleUpdate(node, prop, propagateChange);
-            return <InputBase value={id} onChange={(e) => changeId(e.target.value)} />
-        }
-
-        if (typeof elm[prop] === "object") {
-            return <div> {JSON.stringify(elm[prop])} </div>
-        }
-        else if (elm[prop] === undefined) {
-            return <div />
-        }
-        else {
-            return <div> {elm[prop]} </div>
+                const [id, changeId] = handleUpdate(node, prop, propagateChange);
+                return <InputBase value={id} onChange={(e) => changeId(e.target.value)} />
+            default: return <div>{elm[prop]}</div>
         }
     }
 
