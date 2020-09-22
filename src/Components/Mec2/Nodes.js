dc229e589a26e4603bad3aad138e9ce7ac254451
Kai Lawrence <mail@kailawrence.de>
2020-09-22 19:09:21 +0200

    Implement node id updatability
    Including propagating the change to constraints and views

--- src/Components/Mec2/Nodes.js
+++ src/Components/Mec2/Nodes.js
@@ -6,11 +6,12 @@ export default function Nodes({ mec2, head, elms }) {
     function SanitizedCell({ prop, elm }) {
         const node = mec2._model.nodeById(elm.id);
 
-        function handleUpdate(ref, prop, def = ref[prop]) {
-            const [val, changeVal] = React.useState(def);
+        function handleUpdate(ref, prop, fn) {
+            const [val, changeVal] = React.useState(ref[prop]);
             function handleChange(targetValue) {
                 changeVal(targetValue);
                 ref[prop] = targetValue;
+                if (fn) fn(targetValue);
                 mec2._model.reset();
                 mec2.render();
             };
@@ -27,6 +28,21 @@ export default function Nodes({ mec2, head, elms }) {
         if (prop === 'x' || prop === 'y') {
             const [value, changeValue] = handleUpdate(node, prop);
             return <InputBase value={value} onChange={(e) => changeValue(e.target.value)}/> 
+        }
+
+        if (prop === 'id') {
+            function propagateChange(newId) {
+                mec2._model.constraints.forEach(c => {
+                    if (c.p1 === id) c.p1 = newId;
+                    if (c.p2 === id) c.p2 = newId;
+                });
+                mec2._model.views.forEach(v => {
+                    if (v.of === id) v.of = newId;
+                });
+            }
+
+            const [id, changeId] = handleUpdate(node, prop, propagateChange);
+            return <InputBase value={id} onChange={(e) => changeId(e.target.value)} />
         }
 
         if (typeof elm[prop] === "object") {
