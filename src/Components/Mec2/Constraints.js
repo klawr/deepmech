6bb22a78bc7a57d708e3e0a3bef98d43b913f60e
Kai Lawrence <mail@kailawrence.de>
2020-09-22 20:30:29 +0200

    Make Constraint Ids editable

--- src/Components/Mec2/Constraints.js
+++ src/Components/Mec2/Constraints.js
@@ -1,16 +1,24 @@
 import React from 'react';
-import { MecTable } from '..';
+import { MecTable, handleMecUpdate } from '..';
+import { InputBase } from '@material-ui/core';
 
-export default function Constraints({head, elms}) {
-    function SanitizedCell({elm, prop}) {
-        if (typeof elm[prop] === "object") {
-            return <div> {JSON.stringify(elm[prop])} </div>
+export default function Constraints({ mec2, head, elms }) {
+    function SanitizedCell({ elm, prop }) {
+        const constraint = mec2._model.constraintById(elm.id);
+
+        function handleConstraintUpdate(fn) {
+            return handleMecUpdate(mec2, constraint, prop, fn);
         }
-        else if (prop === undefined) {
-            return <div />
-        }
-        else {
-            return <div> {elm[prop]} </div>
+
+        switch (prop) {
+            case 'id':
+                const [id, changeId] = handleConstraintUpdate();
+                return <InputBase value={id} onChange={(e) => changeId(e.target.value)} />
+            default:
+                if (typeof elm[prop] === 'object') {
+                    return <div> {JSON.stringify(elm[prop])} </div>
+                }
+                return <div>{elm[prop]}</div>
         }
     }
 
