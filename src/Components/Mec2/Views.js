4995619914a7eb61f96f8529a647ffb3b68ec25c
Kai Lawrence <mail@kailawrence.de>
2020-09-23 17:17:17 +0200

    Cleanup Views

--- src/Components/Mec2/Views.js
+++ src/Components/Mec2/Views.js
@@ -1,23 +1,20 @@
 import React from 'react';
 import { MecTable, RadioSelect } from '..';
 
-export default function Views({head, elms}) {
-    function SanitizedCell({prop, elm}) {
-        if (prop === "as") {
-            return <RadioSelect
-                title={"as"}
-                onChange={(p) => console.log(p)}
-                selected={elm[prop]}
-                options={Object.keys(mec.view).filter(e => e !== "extend")} />
-        }
-        if (typeof elm[prop] === "object") {
-            return <div> {JSON.stringify(elm[prop])} </div>
-        }
-        else if (elm[prop] === undefined) {
-            return <div />
-        }
-        else {
-            return <div> {elm[prop]} </div>
+export default function Views({ head, elms }) {
+    function SanitizedCell({ prop, elm }) {
+        switch (prop) {
+            case 'as':
+                return <RadioSelect
+                    title={"as"}
+                    onChange={(p) => console.log(p)}
+                    selected={elm[prop]}
+                    options={Object.keys(mec.view).filter(e => e !== "extend")} />
+            default:
+                if (typeof elm[prop] === "object") {
+                    return <div> {JSON.stringify(elm[prop])} </div>
+                }
+                return <div> {elm[prop]} </div>
         }
     }
 
