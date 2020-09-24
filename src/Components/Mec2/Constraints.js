accab79d7a93693e731ff4452f57fe35c641bf10
Kai Lawrence <mail@kailawrence.de>
2020-09-24 14:29:14 +0200

    Implement change 'len', 'ori' in Constraints

--- src/Components/Mec2/Constraints.js
+++ src/Components/Mec2/Constraints.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import { MecTable, handleMecUpdate, RadioSelect, UpdateText } from '..';
+import { MecTable, handleMecUpdate, RadioSelect, UpdateText, ObjectMenu } from '..';
 
 export default function Constraints({ mec2, head, elms, updateModel }) {
+    // If all are 'free':
+    head.includes('len') || head.push('len');
+    head.includes('ori') || head.push('ori');
+
     function SanitizedCell({ elm, prop }) {
         const constraint = mec2._model.constraintById(elm.id);
 
@@ -21,6 +25,32 @@ export default function Constraints({ mec2, head, elms, updateModel }) {
                     onChange={(val) => changeP(mec2._model.nodeById(val))}
                     selected={p.id}
                     title={prop} />
+            case 'len':
+            case 'ori':
+                elm[prop] = elm[prop] || { type: 'free' };
+                const [obj, changeObj] = handleConstraintUpdate();
+                return <ObjectMenu label={prop} title={elm[prop].type}>
+                    {Object.entries(obj).map(e => {
+                        switch (e[0]) {
+                            case 'type':
+                                return <RadioSelect key={e[0]}
+                                    onChange={(t) => changeObj({ ...obj, [e[0]]: t })}
+                                    title={e[0]}
+                                    label={`${e[0]}: ${e[1]}`}
+                                    selected={e[1]}
+                                    options={['const', 'free', 'drive']} />
+                            case 'ref':
+                                return <RadioSelect key={e[0]}
+                                    onChange={e => changeObj(e.target.value)}
+                                    title={e[0]}
+                                    label={`${prop}: e[1]`}
+                                    selected={e[1]}
+                                    options={mec2._model.nodes.map(n => n.id)} />
+                            default:
+                                return <div>{e[0]}</div>
+                        };
+                    })}
+                </ObjectMenu>
             default:
                 if (typeof elm[prop] === 'object') {
                     return <div> {JSON.stringify(elm[prop])} </div>
