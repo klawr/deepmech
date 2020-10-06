d518e94edc93b07637bed0a0ace3a8832495208b
Kai Lawrence <mail@kailawrence.de>
2020-10-06 10:57:26 +0200

    Initiate UndoRedo with sensible values
    I hope this fixes the initial state on opening on mobile 🤞

--- src/Components/Mec2/UndoRedo.js
+++ src/Components/Mec2/UndoRedo.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
-import { undo, redo, store } from '../../Features';
+import { useSelector, useDispatch } from 'react-redux';
+import { undo, redo, store, selectSelected, selectQueue } from '../../Features';
 import { ListButton } from '..';
 import { Redo, Undo } from '@material-ui/icons';
 import { Grid } from '@material-ui/core';
 
 export default function UndoRedo({ classes }) {
     const dispatch = useDispatch();
+    const queue = useSelector(selectQueue);
+    const selected = useSelector(selectSelected);
 
     // NOTE we assume to start with a clean MecModel.queue:
-    const [undoEnabled, changeUndoEnabled] = React.useState(false);
-    const [redoEnabled, changeRedoEnabled] = React.useState(false);
+    const [undoEnabled, changeUndoEnabled] = React.useState(selected > 0);
+    const [redoEnabled, changeRedoEnabled] = React.useState(selected > queue.length);
 
     React.useEffect(() => store.subscribe(() => {
         // TODO Check if change is related to Model? ...
