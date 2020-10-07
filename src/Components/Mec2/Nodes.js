446ff37507424bb229cb788f9b9594622ebb5c0a
Kai Lawrence <mail@kailawrence.de>
2020-10-07 11:01:24 +0200

    Implement twoway changes

--- src/Components/Mec2/Nodes.js
+++ src/Components/Mec2/Nodes.js
@@ -1,12 +1,48 @@
 import React from 'react';
 import { Checkbox } from '@material-ui/core';
-import { MecTable, handleMecUpdate, UpdateText } from '..';
+import { MecTable, UpdateText } from '..';
 import { useDispatch } from 'react-redux';
 import { add } from '../../Features';
 
 export default function Nodes() {
     const head = ['id', 'x', 'y', 'base'];
     const dispatch = useDispatch();
+
+    React.useEffect(() => {
+        let previous;
+        let value;
+        let selection;
+        function deepmechNodeUp() {
+            if (!value) return;
+            const list = 'nodes';
+            const idx = mecElement._model.nodes.indexOf(selection);
+            dispatch(add({
+                list, idx, property: 'x', value: value.x, previous: previous.x
+            }));
+            dispatch(add({
+                list, idx, property: 'y', value: value.y, previous: previous.y
+            }));
+            previous = undefined;
+            value = undefined;
+        }
+
+        function deepmechNodeDrag(e) {
+            selection = mecElement._selector.selection;
+            if (selection && selection.drag) {
+                previous = previous || { x: e.xusr, y: e.yusr };
+                value = { x: e.xusr, y: e.yusr }
+            }
+        }
+
+        mecElement._interactor.on('drag', deepmechNodeDrag);
+        mecElement._interactor.on('pointerup', deepmechNodeUp);
+
+        return () => {
+            mecElement._interactor.remove('drag', deepmechNodeDrag);
+            mecElement._interactor.remove('pointerup', deepmechNodeUp);
+        }
+    });
+    // store.subscribe(handleMecModelUpdate);
 
     function SanitizedCell({ prop: property, idx, elm }) {
         function update(value, previous = elm[property]) {
