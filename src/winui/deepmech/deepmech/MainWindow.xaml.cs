7dc651e70348fe88de300efea9c57830ab9ececb
Kai Lawrence <mail@kailawrence.de>
2020-11-09 14:23:36 +0100

    First steps to communicate between WinUi and WebView

--- src/winui/deepmech/deepmech/MainWindow.xaml.cs
+++ src/winui/deepmech/deepmech/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-using Microsoft.UI.Xaml;
+﻿using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 
 // To learn more about WinUI, the WinUI project structure,
@@ -11,6 +11,23 @@ namespace deepmech
         public MainWindow()
         {
             InitializeComponent();
+
+            deepmechWebView.WebMessageReceived += (WebView2 sender, WebView2WebMessageReceivedEventArgs args) =>
+            {
+                // Important to validate that the Uri is what we expect from that webview.
+                string uriAsString = sender.Source.ToString();
+
+                if (args.Source == uriAsString)
+                {
+                    deepmechWebView.ExecuteScriptAsync("console.log('Got the message')");
+                }
+
+                else
+                {
+                    // If the source is not validated, don't process the message.
+                    return;
+                }
+            };
         }
     }
 }
