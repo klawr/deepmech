20c603267d7fbd382dc350d0dfb3a63f7dd1219a
Kai Lawrence <mail@kailawrence.de>
2020-11-09 19:10:14 +0100

    Notify WinUI when toggling deepmech

--- src/Components/Deepmech/DeepmechControl.js
+++ src/Components/Deepmech/DeepmechControl.js
@@ -29,6 +29,11 @@ export default function DeepmechControl() {
     }
 
     function toggleDeepmech() {
+        // If the application is run in the WinUI environment this will
+        // toggle the respective UI Elements
+        window.chrome?.webview?.postMessage(
+            JSON.stringify({ deepmech: !UI.deepmech }));
+
         dispatch(UiAction.deepmech(!UI.deepmech));
         if (UI.deepmech) {
             dispatch(mecAction.pause);
