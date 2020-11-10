671c6369c49e336925d9ad4a3a55d610829283b1
Kai Lawrence <mail@kailawrence.de>
2020-11-10 22:49:26 +0100

    Activate inkCanvas in wpf

--- src/wpf/deepmech/MainWindow.xaml.cs
+++ src/wpf/deepmech/MainWindow.xaml.cs
@@ -1,14 +1,28 @@
-﻿using Microsoft.Web.WebView2.Core;
+﻿using Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT;
+using Microsoft.Web.WebView2.Core;
 using System.Windows;
 
 namespace deepmech
 {
     public partial class MainWindow : Window
     {
+        private bool _deepmechActive;
+        public bool deepmechActive
+        {
+            set
+            {
+                deepmechWebView.Width = value ? 0 : double.NaN;
+                _deepmechActive = value;
+            }
+            get { return _deepmechActive; }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
             InitializeAsync();
+            // Set supported inking device types.
+            deepmechCanvas.InkPresenter.InputDeviceTypes = CoreInputDeviceTypes.Mouse | CoreInputDeviceTypes.Pen;
         }
 
         async void InitializeAsync()
@@ -28,7 +42,17 @@ namespace deepmech
                 return;
             }
 
+            var message = System.Text.Json.JsonSerializer.Deserialize<
+                System.Collections.Generic.Dictionary<string, string>>(args.TryGetWebMessageAsString());
+
+            deepmechActive = message?["deepmech"] == "true";
+
             deepmechWebView.ExecuteScriptAsync("console.log(" + args.WebMessageAsJson + ")");
+        }
+
+        private void Exit_Deepmech(object sender, RoutedEventArgs e)
+        {
+            deepmechActive = false;
         }
     }
 }
