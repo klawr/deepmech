e4bd2ab20190b4db702e7971a3d61199ddb89f95
Kai Lawrence <mail@kailawrence.de>
2020-12-09 12:50:28 +0100

    Add constraint detection for mec2 in predict.py

--- src/wpf/deepmech/Resources/predict.py
+++ src/wpf/deepmech/Resources/predict.py
@@ -1,18 +1,18 @@
-﻿from os import path
-
-import os
+﻿import os
 import json
 import cv2
 import tensorflow as tf
 
-srcPath = path.join(os.getcwd(), "..", "..", "..", "Resources");
+srcPath = os.path.join(os.getcwd(), "..", "..", "..", "Resources")
+# For usage from source directory when testing:
+# srcPath = os.path.join(os.path.abspath('.'), "src", "wpf", "deepmech", "Resources")
 
 symbol_detector = tf.keras.models.load_model(
-    path.join(srcPath, 'fcn_sym_det.h5'))
+    os.path.join(srcPath, 'fcn_sym_det.h5'))
 crop_detector = tf.keras.models.load_model(
-    path.join(srcPath, 'crop_detector.h5'))
+    os.path.join(srcPath, 'crop_detector.h5'))
 
-image = cv2.imread(path.join(os.environ["TEMP"], 'deepmechCanvas.png'),
+image = cv2.imread(os.path.join(os.environ["TEMP"], 'deepmechCanvas.png'),
     cv2.IMREAD_GRAYSCALE)
 
 image_tensor = tf.convert_to_tensor(image)
@@ -52,64 +52,59 @@ def get_bounding_boxes_nms(predictions):
 
 elements = {"nodes":[], "constraints":[]}
 
+counter = 1
+
 def appendnode(node, base):
+    global counter
     elements["nodes"].append({
-        "x": int((node[0] + node[2]) / 2 * image_tensor.shape[1]),
-        "y": int((node[1] + node[3]) / 2 * image_tensor.shape[2]),
+        "id": "DM" + str(counter),
+        "x": int((node[1] + node[3]) / 2 * image_tensor.shape[1]),
+        "y": int((node[0] + node[2]) / 2 * image_tensor.shape[2]),
         "base": base,
     })
+    counter += 1
 
 
 [appendnode(node, False) for node in nodes]
 [appendnode(base, True) for base in bases]
 
+crops = []
+crops_info = []
+
+for node1 in elements["nodes"]:
+    for node2 in elements["nodes"]: 
+        x1 = min(node1["x"], node2["x"])
+        y1 = min(node1["y"], node2["y"])
+        x2 = max(node1["x"], node2["x"])
+        y2 = max(node1["y"], node2["y"])
+        # Remove images with area 0
+        if x1 == x2 or y1 == y2:
+            continue
+
+        crop = image[y1:y2, x1:x2]
+        crop = cv2.resize(crop, (96, 96))
+
+        if x1 == node2["x"]:
+            crop = cv2.flip(crop, 1)
+        if y1 == node2["y"]:
+            crop = cv2.flip(crop, 0)
+
+        crop = tf.expand_dims(crop, -1)
+        crop = tf.cast(crop / 255, tf.float32)
+
+        crops.append(crop)
+        n1, n2 = node1["id"], node2["id"]
+        crops_info.append({
+            "p1": n1,
+            "p2": n2,
+            "id": n1 + n2})
+
+for idx, pred in enumerate(crop_detector(tf.convert_to_tensor(crops))):
+    argmax = tf.math.argmax(pred, -1)
+    if (argmax):
+        constraint = crops_info[idx]
+        constraint["len"] = { "type": "const" if argmax == 1 else "free" }
+        constraint["ori"] = { "type": "const" if argmax == 2 else "free" }
+        elements["constraints"].append(constraint)
+
 print(json.dumps(elements))
-
-# box_coords = []
-# for box in [nodes, base_nodes]:
-#     blob = tf.concat([box[0], box[1]], 0)
-#     blob *= 360
-#     blob = tf.cast(blob, tf.int32)
-#     box_coords.append(
-#         [[int((i[0]+i[2])/2),int((i[1]+i[3])/2)] for i in blob])
-
-# crops = []
-# crops_info = []
-# for idx, coords in enumerate(box_coords):
-#     image_crop = []
-#     image_info = []
-#     for node1 in coords:
-#         for node2 in coords:
-#             x1 = min(node1[1], node2[1])
-#             y1 = min(node1[0], node2[0])
-#             x2 = max(node1[1], node2[1])
-#             y2 = max(node1[0], node2[0])
-#             # Remove images with area 0
-#             if x1 == x2 or y1 == y2:
-#                 continue
-
-#             crop = image[y1:y2, x1:x2]
-#             crop = cv2.resize(crop, (96, 96))
-
-#             if x1 == node2[1]:
-#                 x1 = node1[1]
-#                 x2 = node2[1]
-#                 crop = cv2.flip(crop, 1)
-#             if y1 == node2[0]:
-#                 y1 = node1[0]
-#                 y2 = node2[0]
-#                 crop = cv2.flip(crop, 0)
-
-#             crop = tf.expand_dims(crop, -1)
-#             crop = tf.cast(crop / 255, tf.float32)
-
-#             image_crop.append(crop)
-#             image_info.append([x1, y1, x2, y2])
-
-#     crops.append(image_crop)
-#     crops_info.append(image_info)
-
-# crop_pred = [crop_detector(tf.convert_to_tensor(crop)) for crop in crops]
-# constraints = tf.math.argmax(crop_pred, -1)
-
-# print(constraints)
