e65820f12c37bc9773229da3d8742d58c4275436
Kai Lawrence <mail@kailawrence.de>
2021-01-29 20:38:07 +0100

    WIP - WinUI test

---
+++ src/winui/deepmech/deepmech/DeepmechWebView.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Toolkit.Uwp.UI.Controls;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.Web.WebView2.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace deepmech
+{
+    public class DeepmechWebView
+    {
+        WebView2 WebView;
+        InfiniteCanvas Panel;
+
+        private bool _deepmechActive;
+        public bool DeepmechActive
+        {
+            set
+            {
+                WebView.Visibility = value ? Visibility.Collapsed : Visibility.Visible;
+                Panel.Visibility = !value ? Visibility.Collapsed : Visibility.Visible;
+                _deepmechActive = value;
+            }
+            get { return _deepmechActive; }
+        }
+
+        public DeepmechWebView(WebView2 webview, InfiniteCanvas panel)
+        {
+            WebView = webview;
+            Panel = panel;
+        }
+
+        private string WebviewPlaceholder(string message) => "window.webviewEventListenerPlaceholder(" + message + ")";
+
+        public async void ExitDeepmech()
+        {
+            await WebView.ExecuteScriptAsync(WebviewPlaceholder("{deepmech: false}"));
+        }
+
+        public async void SendModelUpdate(string json)
+        {
+            await WebView.ExecuteScriptAsync(WebviewPlaceholder("{update: " + json + "}"));
+        }
+        public void ProcessWebMessage(CoreWebView2 sender, CoreWebView2WebMessageReceivedEventArgs args)
+        {
+            // If the source is not validated, don't process the message.
+            if (args.Source != WebView.Source.ToString()) //sender.Source.ToString()
+            {
+                return;
+            }
+
+            try
+            {
+                var message = System.Text.Json.JsonSerializer.Deserialize<
+                System.Collections.Generic.Dictionary<string, string>>(args.TryGetWebMessageAsString());
+
+                // Toggle canvas if deepmech is active
+                DeepmechActive = message?["deepmech"] == "true";
+            }
+            catch (Exception)
+            {
+                // Can not deserialze message warning or something...
+            }
+        }
+    }
+}
--- src/winui/deepmech/deepmech/MainWindow.xaml.cs
+++ src/winui/deepmech/deepmech/MainWindow.xaml.cs
@@ -1,33 +1,109 @@
 ﻿using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Diagnostics;
+using Microsoft.Web.WebView2.Core;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.IO;
+using Windows.Storage.Streams;
+using Windows.UI.Core;
+
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace deepmech
 {
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
     public sealed partial class MainWindow : Window
     {
+        // private DeepmechPredictionServer DeepmechServer { get; }
+        private DeepmechWebView Communicator { get; }
+
+        private readonly Uri URL = new Uri(Debugger.IsAttached ?
+                "http://localhost:8001" :
+                "https://deepmech.klawr.de");
+
         public MainWindow()
         {
             InitializeComponent();
 
-            deepmechWebView.WebMessageReceived += (WebView2 sender, WebView2WebMessageReceivedEventArgs args) =>
-            {
-                // Important to validate that the Uri is what we expect from that webview.
-                string uriAsString = sender.Source.ToString();
+            deepmechWebView.Source = URL;
+            InitializeAsync();
+            deepmechCanvas.RequiresPointer = RequiresPointer.WhenEngaged;
+            //deepmechCanvas.InkPresenter.InputDeviceTypes = CoreInputDeviceTypes.Mouse | CoreInputDeviceTypes.Pen;
 
-                if (args.Source == uriAsString)
-                {
-                    deepmechWebView.ExecuteScriptAsync("console.log('Got the message')");
-                }
-
-                else
-                {
-                    // If the source is not validated, don't process the message.
-                    return;
-                }
-            };
+            Communicator = new DeepmechWebView(deepmechWebView, deepmechCanvas);
         }
+
+        async void InitializeAsync()
+        {
+            await deepmechWebView.EnsureCoreWebView2Async();
+            deepmechWebView.CoreWebView2.WebMessageReceived += Communicator.ProcessWebMessage;
+
+            if (Debugger.IsAttached)
+            {
+                deepmechWebView.CoreWebView2.OpenDevToolsWindow();
+            }
+        }
+
+        public void ExitDeepmech(object sender, RoutedEventArgs e)
+        {
+            Communicator.ExitDeepmech();
+        }
+
+        private async void Predict(object sender, RoutedEventArgs e)
+        {
+            var image = await ReadDeepmechCanvas();
+            if (image == null)
+            {
+                throw new Exception("Canvas contains no image.");
+            };
+            var prediction = HelloTf.Predict(image);
+            Communicator.SendModelUpdate(prediction);
+        }
+
+
+        private async Task<Image<A8>> ReadDeepmechCanvas()
+        {
+            var strokes = deepmechCanvas.ExportInkStrokes();
+            if (strokes.Count == 0) return null;
+
+            // Make strokes white
+            var attributes = new List<Windows.UI.Input.Inking.InkDrawingAttributes>();
+            foreach (var stroke in strokes)
+            {
+                attributes.Add(stroke.DrawingAttributes);
+                // Make all strokes black
+                var DA = stroke.Clone().DrawingAttributes;
+                DA.Color = Windows.UI.Color.FromArgb(0, 255, 255, 255);
+                stroke.DrawingAttributes = DA;
+            }
+
+
+
+            Image<A8> image;
+            // Draw strokes and encode image into base64 string
+            using (var stream = new InMemoryRandomAccessStream())
+            {
+                await deepmechCanvas.SaveBitmapAsync(stream, 0);
+                image = SixLabors.ImageSharp.Image.Load<A8>(stream.AsStream());
+            }
+
+
+            // Revert color of strokes.
+            for (var i = 0; i < strokes.Count; ++i)
+            {
+                strokes[i].DrawingAttributes = attributes[i];
+            }
+
+            return image;
+        }
+
     }
 }
