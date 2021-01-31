bd7a9e962377b5914720ca1dc7a05766d487d6b3
Kai Lawrence <mail@kailawrence.de>
2021-01-31 01:06:52 +0100

    Combine WPF with CC library

---
+++ src/wpf/deepmech/DeepmechWebView.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Web.WebView2.Wpf;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Web.WebView2.Core;
+
+namespace deepmech
+{
+    class DeepmechWebView
+    {
+        WebView2 WebView;
+
+        private bool _deepmechActive;
+        public bool DeepmechActive
+        {
+            set
+            {
+                WebView.Width = value ? 0 : double.NaN;
+                _deepmechActive = value;
+            }
+            get { return _deepmechActive; }
+        }
+
+        public DeepmechWebView(WebView2 webview)
+        {
+            WebView = webview;
+        }
+
+        private string WebviewPlaceholder(string message) => "globalThis.webviewEventListenerPlaceholder(" + message + ")";
+
+        public void ExitDeepmech()
+        {
+            WebView.ExecuteScriptAsync(WebviewPlaceholder("{deepmech: false}"));
+        }
+
+        public void SendModelUpdate(string json)
+        {
+            WebView.ExecuteScriptAsync(WebviewPlaceholder("{prediction: " + json + "}"));
+        }
+
+        public void Register(bool canvas, bool prediction)
+        {
+            WebView.ExecuteScriptAsync(WebviewPlaceholder("{register:{canvas: false, prediction: true}}"));
+        }
+
+        public void ProcessWebMessage(object sender, CoreWebView2WebMessageReceivedEventArgs e)
+        {
+            // If the source is not validated, don't process the message.
+            if (e.Source != WebView.Source.ToString()) //sender.Source.ToString()
+            {
+                return;
+            }
+
+            // TODO try serialize...
+            var message = System.Text.Json.JsonSerializer.Deserialize<Dictionary<string, string>>(e.TryGetWebMessageAsString());
+
+            if (message == null) return;
+            if (message.TryGetValue("ready", out var ready) && ready == "true")
+            {
+                Register(canvas: false, prediction: true);
+            }
+            if (message.TryGetValue("image", out var base64))
+            {
+                WebView.ExecuteScriptAsync(WebviewPlaceholder($"{{prediction: {Deepmech_cc.Predict(base64)}}}"));
+            }
+        }
+    }
+}
--- src/wpf/deepmech/MainWindow.xaml.cs
+++ src/wpf/deepmech/MainWindow.xaml.cs
@@ -9,21 +9,15 @@ using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows;
 using Windows.Storage.Streams;
+using System.Net.Http;
+using Microsoft.Web.WebView2.Wpf;
+using System.Net.Http.Json;
 
 namespace deepmech
 {
     public partial class MainWindow : Window
     {
-        private bool _deepmechActive;
-        public bool DeepmechActive
-        {
-            set
-            {
-                deepmechWebView.Width = value ? 0 : double.NaN;
-                _deepmechActive = value;
-            }
-            get { return _deepmechActive; }
-        }
+        private DeepmechWebView Communicator { get; }
 
         private readonly Uri URL = new Uri(Debugger.IsAttached ?
                 "http://localhost:8001" :
@@ -31,16 +25,18 @@ namespace deepmech
 
         public MainWindow()
         {
+
             InitializeComponent();
             deepmechWebView.Source = URL;
+            Communicator = new DeepmechWebView(deepmechWebView);
+
             InitializeAsync();
-            deepmechCanvas.InkPresenter.InputDeviceTypes = CoreInputDeviceTypes.Mouse | CoreInputDeviceTypes.Pen;
         }
 
         async void InitializeAsync()
         {
             await deepmechWebView.EnsureCoreWebView2Async(null);
-            deepmechWebView.CoreWebView2.WebMessageReceived += ProcessWebMessage;
+            deepmechWebView.CoreWebView2.WebMessageReceived += Communicator.ProcessWebMessage;
 
             if (Debugger.IsAttached)
             {
@@ -48,107 +44,14 @@ namespace deepmech
             }
         }
 
-        void ProcessWebMessage(object sender, CoreWebView2WebMessageReceivedEventArgs args)
-        {
-            // If the source is not validated, don't process the message.
-            if (args.Source != deepmechWebView.Source.ToString()) //sender.Source.ToString()
-            {
-                return;
-            }
+        //private void ExitDeepmech(object sender, RoutedEventArgs e)
+        //{
+        //    Communicator.ExitDeepmech();
+        //}
 
-            try
-            {
-                var message = System.Text.Json.JsonSerializer.Deserialize<
-                System.Collections.Generic.Dictionary<string, string>>(args.TryGetWebMessageAsString());
-
-                // Toggle canvas if deepmech is active
-                DeepmechActive = message?["deepmech"] == "true";
-            }
-            catch (Exception)
-            {
-                // Can not deserialze message warning or something...
-            }
-        }
-
-        // This is not as described in
-        // https://docs.microsoft.com/en-us/microsoft-edge/webview2/gettingstarted/winforms#step-8---communication-between-host-and-web-content
-        // But it does not work that way ¯\_(ツ)_/¯
-        private string WebviewPlaceholder(string message) => "window.webviewEventListenerPlaceholder(" + message + ")";
-
-        private void ExitDeepmech(object sender, RoutedEventArgs e)
-        {
-            deepmechWebView.ExecuteScriptAsync(WebviewPlaceholder("{deepmech: false}"));
-        }
-
-        private async Task ReadDeepmechCanvas(string path)
-        {
-            var strokes = deepmechCanvas.InkPresenter.StrokeContainer.GetStrokes();
-            var tmp = Path.GetTempFileName();
-            if (strokes.Count > 0)
-            {
-                // Make strokes white
-                var attributes = new List<Windows.UI.Input.Inking.InkDrawingAttributes>();
-                foreach (var stroke in strokes)
-                {
-                    attributes.Add(stroke.DrawingAttributes);
-                    var DA = stroke.Clone().DrawingAttributes;
-                    DA.Color = Windows.UI.Color.FromArgb(0, 255, 255, 255);
-                    stroke.DrawingAttributes = DA;
-                }
-                // Draw strokes into temporary file
-                using (IOutputStream outputStream = File.Open(tmp, FileMode.Create).AsOutputStream())
-                {
-                    await deepmechCanvas.InkPresenter.StrokeContainer.SaveAsync(outputStream);
-                    await outputStream.FlushAsync();
-                }
-                // Make image usable for opencv-python (cv2)
-                // It seems that the temporary file lags png encoding (?) So it is done here explictitly
-                Image.FromFile(tmp).Save(path, System.Drawing.Imaging.ImageFormat.Png);
-
-                for (var i = 0; i < strokes.Count; ++i)
-                {
-                    strokes[i].DrawingAttributes = attributes[i];
-                }
-            }
-        }
-
-        public string RunPythonScript(string pythonPath, string scriptPath)
-        {
-            using var process = new Process();
-            process.StartInfo = new ProcessStartInfo(pythonPath)
-            {
-                Arguments = scriptPath,
-                UseShellExecute = false,
-                RedirectStandardOutput = true,
-                RedirectStandardError = true,
-                CreateNoWindow = true
-            };
-            process.Start();
-            var output = process.StandardOutput.ReadToEnd();
-            var error = process.StandardError.ReadToEnd(); // Do something with error?
-            process.WaitForExit();
-
-            return output;
-        }
-
-        private void SendModelUpdate(string json)
-        {
-            deepmechWebView.ExecuteScriptAsync(WebviewPlaceholder("{update: " + json + "}"));
-        }
-
-        private async void Predict(object sender, RoutedEventArgs e)
-        {
-            var sourcePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..", "..", "Resources");
-            // Get image
-            await ReadDeepmechCanvas(Path.Combine(Path.GetTempPath(), "deepmechCanvas.png"));
-
-            // Call python with image
-            var Prediction = RunPythonScript(
-                Path.Combine(sourcePath, "env", "Scripts", "python.exe"),
-                Path.Combine(sourcePath, "predict.py"));
-
-            // Pass predictions to mec2
-            SendModelUpdate(Prediction);
-        }
+        //private void Predict(object sender, RoutedEventArgs e)
+        //{
+        //    Communicator.Predict()
+        //}
     }
 }
