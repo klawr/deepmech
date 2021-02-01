d026d1719cfede5cfd9eaf9137f361475dc95254
Kai Lawrence <mail@kailawrence.de>
2021-02-01 16:28:40 +0100

    Implement ignoring existing nodes

--- src/wpf/deepmech/DeepmechWebView.cs
+++ src/wpf/deepmech/DeepmechWebView.cs
@@ -5,12 +5,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Web.WebView2.Core;
+using System.Text.Json;
+using System.Reflection;
+using System.IO;
 
 namespace deepmech
 {
     class DeepmechWebView
     {
         WebView2 WebView;
+        IntPtr Deepmech_ctx;
 
         private bool _deepmechActive;
         public bool DeepmechActive
@@ -26,6 +30,11 @@ namespace deepmech
         public DeepmechWebView(WebView2 webview)
         {
             WebView = webview;
+            var a = Properties.Resources.fcn_sym_det;
+            var symbolModel = Encoding.UTF8.GetString(a, 0, a.Length);
+            var b = Properties.Resources.crop_detector;
+            var cropModel = Encoding.UTF8.GetString(b, 0, b.Length);
+            Deepmech_ctx = Deepmech_cxx.create_deepmech_ctx(symbolModel, cropModel);
         }
 
         private string WebviewPlaceholder(string message) => "globalThis.webviewEventListenerPlaceholder(" + message + ")";
@@ -45,6 +54,19 @@ namespace deepmech
             WebView.ExecuteScriptAsync(WebviewPlaceholder("{register:{canvas: false, prediction: true}}"));
         }
 
+        private void Predict(string prediction)
+        {
+            WebView.ExecuteScriptAsync(WebviewPlaceholder($"{{prediction: {prediction}}}"));
+        }
+
+        private class DeepmechMessage
+        {
+            public string image { get; set; }
+            public bool ready { get; set; }
+            public uint[] nodes { get; set; }
+        }
+
+
         public void ProcessWebMessage(object sender, CoreWebView2WebMessageReceivedEventArgs e)
         {
             // If the source is not validated, don't process the message.
@@ -53,17 +75,16 @@ namespace deepmech
                 return;
             }
 
-            // TODO try serialize...
-            var message = System.Text.Json.JsonSerializer.Deserialize<Dictionary<string, string>>(e.TryGetWebMessageAsString());
+            var message = JsonSerializer.Deserialize<DeepmechMessage>(e.TryGetWebMessageAsString());
 
             if (message == null) return;
-            if (message.TryGetValue("ready", out var ready) && ready == "true")
+            if (message.ready)
             {
                 Register(canvas: false, prediction: true);
             }
-            if (message.TryGetValue("image", out var base64))
+            if (message.image != null)
             {
-                WebView.ExecuteScriptAsync(WebviewPlaceholder($"{{prediction: {Deepmech_cc.Predict(base64)}}}"));
+                Predict(Deepmech_cxx.Predict(Deepmech_ctx, message.image, message.nodes));
             }
         }
     }
--- src/wpf/deepmech/Properties/Resources.Designer.cs
+++ src/wpf/deepmech/Properties/Resources.Designer.cs
@@ -22,7 +22,7 @@ namespace deepmech.Properties {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources {
+    public class Resources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace deepmech.Properties {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("deepmech.Properties.Resources", typeof(Resources).Assembly);
@@ -51,12 +51,32 @@ namespace deepmech.Properties {
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
             set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        public static byte[] crop_detector {
+            get {
+                object obj = ResourceManager.GetObject("crop_detector", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        public static byte[] fcn_sym_det {
+            get {
+                object obj = ResourceManager.GetObject("fcn_sym_det", resourceCulture);
+                return ((byte[])(obj));
             }
         }
     }
