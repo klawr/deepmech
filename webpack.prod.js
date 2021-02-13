b62c4f0c72206e1ed93d11cd042ae17047cab9b4
Kai Lawrence <mail@kailawrence.de>
2021-02-13 01:58:32 +0100

    Update webpack.prod and .gitignore

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,22 +1,8 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
 const MinifyPlugin = require("babel-minify-webpack-plugin");
 
 module.exports = merge(common, {
-    mode: 'production',
-    optimization: {
-        //sideEffects: true,
-        //usedExports: true,
-        /*splitChunks: {
-            chunks: 'all',
-        },*/
-        //moduleIds: "size",
-        //chunkIds: "size",
-    },
-    plugins: [
-        new MinifyPlugin({}, {
-            //"comments": false
-        })
-    ]
+  mode: "production",
+  plugins: [new MinifyPlugin()],
 });
-
