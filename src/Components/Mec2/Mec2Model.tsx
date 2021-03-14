afd052f6d5701b8ad982fbcfdd44cee13c4b572c
Kai Lawrence <mail@kailawrence.de>
2021-03-14 12:59:36 +0100

    Move Mec2/Properties into Mec2/ModelProperties

--- src/Components/Mec2/Mec2Model.tsx
+++ src/Components/Mec2/Mec2Model.tsx
@@ -1,8 +1,8 @@
 import { Grid } from '@material-ui/core';
-import Mec2Id from './Properties/Mec2Id';
-import Mec2Nodes from './Properties/Mec2Nodes';
-import Mec2Constraints from './Properties/Mec2Constraints';
-import Mec2Views from './Properties/Mec2Views';
+import Mec2Id from './ModelProperties/Mec2Id';
+import Mec2Nodes from './ModelProperties/Mec2Nodes';
+import Mec2Constraints from './ModelProperties/Mec2Constraints';
+import Mec2Views from './ModelProperties/Mec2Views';
 import Mec2UndoRedo from './Utils/Mec2UndoRedo';
 
 export default function Mec2Model() {
