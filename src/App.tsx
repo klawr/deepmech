01ea62669ba7cb401374c4cf8779eb69bc7dc326
Kai Lawrence <mail@kailawrence.de>
2021-03-14 12:41:00 +0100

    Move handleMecModelUpdate into Services

--- src/App.tsx
+++ src/App.tsx
@@ -8,75 +8,11 @@ import { UIAction } from './Redux/UISlice';
 import store from './Redux/store';
 import RightDrawer from './Components/RightDrawer';
 import mecElementSingleton from './Services/Singletons/mecElement';
-import { IMecModelState } from './Redux/MecModelSlice';
-import { IMecProperty } from './Services/Singletons/mecModel';
+import handleMecModelUpdate from './Services/handleMecModelUpdate';
 
 const ref = mecElementSingleton();
 
-let mecModel: IMecModelState | undefined;
-let counter = 0;
-
-function handleMecModelUpdate() {
-  if (mecModel === store.getState().MecModel) return;
-  mecModel = store.getState().MecModel;
-  // if true the last action was an update, otherwise it was an undo
-  const up = counter < mecModel.selected;
-  const action = up
-    ? mecModel.queue[mecModel.selected - 1]
-    : mecModel.queue[mecModel.selected];
-  if (!action) return;
-
-  const list = ref._model[action.list];
-
-  if (typeof action.idx === "number") {
-    Object.entries(up ? action.value : action.previous).forEach((e: [string, IMecProperty]) => {
-      const element = list[action.idx as number];
-      (element as any)[e[0]] = e[1]; // sigh...
-    });
-  } else if (action.idx === "add" || action.idx === "remove") {
-    // Check if element is going to be added (or removed)
-    const add =
-      (up && action.idx === "add") || (!up && action.idx === "remove");
-    if (action.list === "nodes" ||
-      action.list === "constraints" ||
-      action.list === "views") {
-      const element = { ...action.value } as IMecProperty;
-      if (add) {
-        if ((list as IMecProperty[]).find((e) => e.id === element.id)) {
-          // console.warn(`Can not add element to ${action.list}. Id ${element.id} is already taken`)
-          return;
-        }
-        ref._model.plugIns[action.list].extend(element);
-        ref._model.add(action.list, element);
-        element.init(ref._model);
-      } else {
-        // TODO why is this ? necessary?
-        const o = (list as IMecProperty[]).find((e) => e.id === element.id);
-        if (o) o.remove();
-      }
-    }
-    // TODO this causes some issues...
-    ref._model.draw(ref._g);
-  }
-
-  if (action.list === "constraints") {
-    // Skip updating nodes when the constraint is removed...
-    if (action.idx === "remove" && up) return;
-
-    ref._model.constraints.forEach((e: any) => e.assignRefs());
-  }
-  if (action.list === "views") {
-    ref._model.views.forEach((e: any) => e.assignRefs());
-  }
-
-  ref._model.preview();
-  ref._model.pose();
-  ref.render();
-
-  counter = mecModel.selected;
-}
-
-store.subscribe(handleMecModelUpdate);
+store.subscribe(() => handleMecModelUpdate(store, ref));
 
 // Let g2 beg simulate view (beg does not respect cartesian)
 function begSimView(v: any) {
---
+++ src/Services/handleMecModelUpdate.ts
@@ -0,0 +1,69 @@
+import { EnhancedStore } from "@reduxjs/toolkit";
+import { IMecModelState } from "../Redux/MecModelSlice";
+import { IMecElement } from "./Singletons/mecElement";
+import { IMecProperty } from './Singletons/mecModel';
+
+let mecModel: IMecModelState | undefined;
+let counter = 0;
+
+export default function handleMecModelUpdate(store: EnhancedStore, ref: IMecElement) {
+    // if mecModel did not change from last time, return
+    if (mecModel === store.getState().MecModel) return;
+    // Can't be undefined if false.
+    mecModel = store.getState().MecModel as IMecModelState;
+    // if true the last action was an update, otherwise it was an undo
+    const up = counter < mecModel.selected;
+    const action = up
+        ? mecModel.queue[mecModel.selected - 1]
+        : mecModel.queue[mecModel.selected];
+    if (!action) return;
+
+    const list = ref._model[action.list];
+
+    if (typeof action.idx === "number") {
+        Object.entries(up ? action.value : action.previous).forEach((e: [string, IMecProperty]) => {
+            const element = list[action.idx as number];
+            (element as any)[e[0]] = e[1]; // sigh...
+        });
+    } else if (action.idx === "add" || action.idx === "remove") {
+        // Check if element is going to be added (or removed)
+        const add =
+            (up && action.idx === "add") || (!up && action.idx === "remove");
+        if (action.list === "nodes" ||
+            action.list === "constraints" ||
+            action.list === "views") {
+            const element = { ...action.value } as IMecProperty;
+            if (add) {
+                if ((list as IMecProperty[]).find((e) => e.id === element.id)) {
+                    // console.warn(`Can not add element to ${action.list}. Id ${element.id} is already taken`)
+                    return;
+                }
+                ref._model.plugIns[action.list].extend(element);
+                ref._model.add(action.list, element);
+                element.init(ref._model);
+            } else {
+                // TODO why is this ? necessary?
+                const o = (list as IMecProperty[]).find((e) => e.id === element.id);
+                if (o) o.remove();
+            }
+        }
+        // TODO this causes some issues...
+        ref._model.draw(ref._g);
+    }
+
+    if (action.list === "constraints") {
+        // Skip updating nodes when the constraint is removed...
+        if (action.idx === "remove" && up) return;
+
+        ref._model.constraints.forEach((e: any) => e.assignRefs());
+    }
+    if (action.list === "views") {
+        ref._model.views.forEach((e: any) => e.assignRefs());
+    }
+
+    ref._model.preview();
+    ref._model.pose();
+    ref.render();
+
+    counter = mecModel.selected;
+}
