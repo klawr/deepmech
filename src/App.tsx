5f0ac0300445fd3220fed57b11a1c5a2a10bec5a
Kai Lawrence <mail@kailawrence.de>
2021-03-14 23:45:34 +0100

    Reimplement darkmode

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import './App.css';
 import LeftDrawer from './Components/LeftDrawer';
-import { Grid } from '@material-ui/core';
+import { createMuiTheme, Grid, MuiThemeProvider } from '@material-ui/core';
 import ListButton from './Components/Utils/ListButton';
 import { ChevronLeft, ChevronRight } from '@material-ui/icons';
 import { useDispatch, useSelector } from 'react-redux';
-import { UIAction } from './Redux/UISlice';
+import { UIAction, UISelect } from './Redux/UISlice';
 import store from './Redux/store';
 import RightDrawer from './Components/RightDrawer';
 import mecElementSingleton from './Services/Singletons/mecElement';
@@ -14,36 +14,45 @@ import DeepmechCanvas from './Components/Deepmech/DeepmechCanvas';
 import { mecModelAction } from './Redux/MecModelSlice';
 
 const ref = mecElementSingleton();
+const lightTheme = createMuiTheme({ palette: { type: 'light' } });
+const darkTheme = createMuiTheme({ palette: { type: 'dark' } });
 
 store.subscribe(() => handleMecModelUpdate(store, ref));
 
 export default function App() {
   const dispatch = useDispatch();
+
   const deepmech = useSelector(deepmechSelect);
+  const UI = useSelector(UISelect);
 
   dispatch(mecModelAction.initialize());
 
   return (
-    <div className="root">
-      <LeftDrawer />
-      <RightDrawer />
-      {deepmech.active && <DeepmechCanvas />}
-      <Grid
-        className="buttonGrid"
-        container
-        direction="row">
-        <ListButton
-          onClick={() => dispatch(UIAction.left(true))}
-          tooltip="Open drawer">
-          <ChevronRight />
-        </ListButton>
-        <ListButton
-          className="rightDrawerButton"
-          onClick={() => dispatch(UIAction.right(true))}
-          tooltip="Open right drawer">
-          <ChevronLeft />
-        </ListButton>
-      </Grid>
-    </div>
+    <MuiThemeProvider theme={UI.darkmode ? darkTheme : lightTheme}>
+      <div className="root">
+        <MuiThemeProvider theme={UI.darkmode ? darkTheme : lightTheme}>
+          <LeftDrawer />
+          <RightDrawer />
+          {deepmech.active && <DeepmechCanvas />}
+          <Grid
+            className="buttonGrid"
+            container
+            direction="row">
+
+            <ListButton
+              onClick={() => dispatch(UIAction.left(true))}
+              tooltip="Open drawer">
+              <ChevronRight />
+            </ListButton>
+            <ListButton
+              className="rightDrawerButton"
+              onClick={() => dispatch(UIAction.right(true))}
+              tooltip="Open right drawer">
+              <ChevronLeft />
+            </ListButton>
+          </Grid>
+        </MuiThemeProvider>
+      </div>
+    </MuiThemeProvider>
   );
 }
