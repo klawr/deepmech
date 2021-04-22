dddfdc595775e16b83a15f0f7b0d426ebda2c2db
Kai Lawrence <mail@kailawrence.de>
2021-04-22 14:26:32 +0200

    Implement drawing in Deepmech

--- src/react-native/src/Components/Deepmech/Deepmech.tsx
+++ src/react-native/src/Components/Deepmech/Deepmech.tsx
@@ -1,13 +1,36 @@
 import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Animated, StyleSheet, PanResponder, Text, View } from 'react-native';
+import { Svg, Path } from 'react-native-svg';
 import Header from '../Header';
 
-export default function Mec2({ navigation } = {} as any) {
+export default function Deepmech({ navigation } = {} as any) {
+    const [path, setPath] = React.useState("");
+
+    const panResponder = PanResponder.create({
+        onMoveShouldSetPanResponder: () => true,
+        onPanResponderGrant: (_, gestureState) => {
+            setPath(path + `M${gestureState.moveX} ${gestureState.moveY} `);
+        },
+        onPanResponderMove: (_, gestureState) => {
+            setPath(path + `L${gestureState.moveX} ${gestureState.moveY} `);
+        },
+        onPanResponderRelease: () => { }
+    });
+
     return <View style={{ flex: 1 }}>
-        <Text>Hello Deepmech</Text>
+        <Animated.View style={{ flex: 1 }} {...panResponder.panHandlers}>
+            <Svg style={{ flex: 1 }}>
+                <Path
+                    d={path}
+                    stroke="black"
+                    fill="none"
+                    strokeLinecap='round'
+                    strokeWidth={3} />
+            </Svg>
+        </Animated.View>
+
         <Header navigation={navigation} />
     </View >
 }
 
-const styles = StyleSheet.create({
-});
+const styles = StyleSheet.create({});
