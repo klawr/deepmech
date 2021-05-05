5976457ec101a0c522f27f29a7cc49aa90266e16
Kai Lawrence <mail@kailawrence.de>
2021-05-05 14:06:48 +0200

    Implement pytorch lib and add sample bitmap load

--- src/react-native/native_modules/react-native-deepmech/android/src/main/java/com/reactlibrary/ReactNativeDeepmechModule.java
+++ src/react-native/native_modules/react-native-deepmech/android/src/main/java/com/reactlibrary/ReactNativeDeepmechModule.java
@@ -7,13 +7,36 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 
+// import java.io.File;
+// import java.io.FileOutputStream;
+import java.io.IOException;
+// import java.io.InputStream;
+// import java.io.OutputStream;
+
+import android.util.Log;
+// import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+// import org.pytorch.Module;
+// import org.pytorch.Tensor;
+
 public class ReactNativeDeepmechModule extends ReactContextBaseJavaModule {
 
     private final ReactApplicationContext reactContext;
 
+    private Bitmap bitmap;
+    // private Module symbol_detector;
+
     public ReactNativeDeepmechModule(ReactApplicationContext reactContext) {
         super(reactContext);
         this.reactContext = reactContext;
+        try {
+            this.bitmap = BitmapFactory.decodeStream(reactContext.getAssets().open("image.png"));
+        } catch (IOException e) {
+             Log.e("PytorchHelloWorld", "Error reading assets", e);
+        }
+        //  this.symbol_detector = Module.load(assetFilePath(reactContext, "symbol_detector.pth"));
     }
 
     @Override
@@ -23,7 +46,34 @@ public class ReactNativeDeepmechModule extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void sample(String str, Callback cb) {
+        final int Height = this.bitmap.getByteCount();
+        // final Module module = Module.load(assetFilePath(this, "symbol_detector.pth"));
+
         // TODO: Implement some actually useful functionality
-        cb.invoke(str + "world");
+        cb.invoke(str + "world" + String.valueOf(Height));
     }
+
+    /**
+     * Copies specified asset to the file in /files app directory and  returns this file absolute path.
+     *
+     * @return absolute file path
+     */
+    // public static String assetFilePath(Context context, String assetName) throws IOException {
+    //     File file = new File(context.getFilesDir(), assetName);
+    //     if (file.exists() && file.length() > 0) {
+    //         return file.getAbsolutePath();
+    //     }
+
+    //     try (InputStream is = context.getAssets().open(assetName)) {
+    //         try (OutputStream os = new FileOutputStream(file)) {
+    //             byte[] buffer = new byte[4 * 1024];
+    //             int read;
+    //             while ((read = is.read(buffer)) != -1) {
+    //                 os.write(buffer, 0, read);
+    //             }
+    //             os.flush();
+    //         }
+    //         return file.getAbsolutePath();
+    //     }
+    // }
 }
